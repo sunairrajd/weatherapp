0c15d436936166740ece35af406603d8062f130e
Sunair Raj <sunairraj@Sunairs-MacBook-Pro.local>
2020-09-21 11:01:47 +0530

    f

--- v2/sketchb.js
+++ v2/sketchb.js
@@ -167,20 +167,24 @@ function setup() {
 angleMode(DEGREES);
 
   // Define colors
-  a1 = color(230,130,99);
-  a2 = color(247,212,98);
+  a1 = color(252,87,0);
+  a2 = color(255,179,5);
 
-  b1 = color(0,59,255);
-  b2 = color(126,0,252);
+  b1 = color(254,174,4);
+  b2 = color(120,2,252);
 
-  c1 = color(230,130,99);
-  c2 = color(247,212,98);
+  c1 = color(132,18,227);
+  c2 = color(4,49,198);
 
-  d1 = color(95,74,211);
-  d2 = color(19,14,141);
+  d1 = color(15,57,227);
+  d2 = color(5,1,101);
 
-  e1 = color(27,4,160);
-  e2 = color(12,28,82);
+  e1 = color(5,1,102);
+  e2 = color(100,0,170);
+
+
+  f1 = color(100,0,170);
+  f2 = color(252,87,0);
 
 
 
@@ -242,37 +246,41 @@ function gotData(data) {
 
     loc = weather.name;
     time = weather.dt;
-    timezone = weather.timezone;
-    minutesshifttimeuser = (timezone/60);
-
-    var date = new Date(time * 1000);
-    var month = date.getMonth();
-    var timezoneoffset = date.getTimezoneOffset();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-
-    var totalminutes = (hours*60) + minutes;
-    var minutesatutclocation = totalminutes + timezoneoffset;
-    var minutesatinputlocation= minutesatutclocation + minutesshifttimeuser ;
-    var hoursatinputlocation = Math.floor(minutesatinputlocation/60);
-
-    console.log('timezone' + timezoneoffset);
-    console.log('timezonediffuser' + minutesshifttimeuser);
-    console.log('hours' + hours);
-    console.log('miutes' + minutes);
-    console.log('totalminutes' + totalminutes);
-    console.log('hoursatinputlocation' + hoursatinputlocation);
 
 
+  var date = new Date(time * 1000);
+  var timezone = weather.timezone;
+  //var minutesshifttimeuser = (timezone/60);
+
+  var utcstamp = Math.floor((new Date()).getTime() / 1000);
+  var utcatlocation = utcstamp + timezone;
+  dateObj = new Date(utcatlocation * 1000);
+  utcString = dateObj.toUTCString();
+  hourslice = (utcString.slice(-12, -10))*1;
 
 
-  //  console.log(month);
+//console.log(timezone);
+
+//console.log(utcatlocation);
+//console.log(utcString);
+    console.log(hourslice);
+
+
+    //var hours = date.getHours();
+    //var minutes = date.getMinutes();
+
+
+
+
+
+
     feelslike = weather.main.feels_like;
-    feelslikec = (feelslike-273.15)*1.8+32;
-    //feelslikec = (Math.round(feelslike-273.15));
+    feelslikef = (feelslike-273.15)*1.8+32;
+    feelslikec = (Math.round(feelslike-273.15));
     //feelslikecircle = Math.log2(feelslikec)*15;
-    feelslikecircle = feelslikec*0.5;
-    console.log('tempppppp in f ' + feelslikec);
+    feelslikecircle = feelslikef*0.5;
+    console.log('tempppppp in f ' + feelslikef);
+    console.log('tempppppp in c ' + feelslikec);
 
 
 
@@ -291,36 +299,44 @@ function gotData(data) {
 
   function mytime() {
 
-    if (hoursatinputlocation >= 0 && hoursatinputlocation < 5){
-      samay = 'night';
+
+
+     if (hourslice >= 0 && hourslice < 5){
+      samay = 'mid-night';
+      setGradient(d1, d2);
+    }
+
+    else if (hourslice >= 5 && hourslice < 7){
+      samay = 'mid-night';
       setGradient(e1, e2);
     }
 
-    else if (hoursatinputlocation >= 6 && hoursatinputlocation < 12){
+    else if (hourslice >= 7 && hourslice < 8){
+       samay = 'early morning';
+       setGradient(f1, f2);
+    }
+
+    else if (hourslice >= 8 && hourslice < 12) {
        samay = 'morning';
-       setGradient(a1, a2);
+       setGradient(f1, f2);
     }
 
-    else if (hoursatinputlocation >= 12 && hoursatinputlocation < 15) {
+    else if (hourslice >= 12 && hourslice < 15) {
        samay = 'afternoon';
-       setGradient(c1, c2);
+        setGradient(a1, a2);
     }
 
-    else if (hoursatinputlocation >= 15 && hoursatinputlocation < 18) {
+    else if (hourslice >= 15 && hourslice < 19) {
        samay = 'evening';
-        setGradient(c1, c2);
+       setGradient(b1, b2);
     }
 
-    else if (hoursatinputlocation >= 18 && hoursatinputlocation < 22) {
+    else if (hourslice >= 19 && hourslice < 24) {
        samay = 'night';
-       setGradient(d1, d2);
+       setGradient(c1,c2);
     }
-
-    else if (hoursatinputlocation >= 22 && hoursatinputlocation < 24) {
-       samay = 'night';
-       setGradient(e1,e2);
-    }
-     else { samay = 'nnt';
+     else {
+      samay = 'not working';
      setGradient(b1,b2);
     }
     return samay;
@@ -328,7 +344,7 @@ function gotData(data) {
 
 
 
-              mymonth = date.toLocaleString('default', { month: 'long' })
+              mymonth = date.toLocaleString('default', { month: 'long' });
 
     var message = 'As im writing you this '  + mymonth.toLowerCase() + ' ' + mytime()  +  '. The weather outside is ' +  main + 'y, ' + cloudsfun(clouds)+ ' but it feels ' + cold(feelslikec);
 
@@ -357,39 +373,51 @@ for(var x= -1000; x<6500; x=x+winSpeline) {
 
 
 
-  if (hoursatinputlocation>6 && hoursatinputlocation<18) {
+  if (hourslice>=7 && hourslice<=19) {
     push();
     noStroke();
     blendMode(SOFT_LIGHT);
-    fill(0,clouds*0.01*255*0.9);
+    fill(0,clouds*0.01*255);
     circle(random(0,800),0, random(clouds*3,clouds*8));
     circle(0,random(0,480), random(clouds*3,clouds*8));
     circle(800,random(0,480), random(clouds*3,clouds*8));
     circle(random(0,800),480, random(clouds*3,clouds*8));
     pop();
+    console.log('im putting black clouds');
   }
   else {
 
     push();
     noStroke();
     blendMode(SOFT_LIGHT);
-    fill(255,clouds*0.01*255*0.9);
+    fill(255,clouds*0.01*255);
     circle(random(0,800),0, random(clouds*3,clouds*8));
     circle(0,random(0,480), random(clouds*3,clouds*8));
     circle(800,random(0,480), random(clouds*3,clouds*8));
     circle(random(0,800),480, random(clouds*3,clouds*8));
     pop();
+    console.log('im putting white clouds');
   }
 //return cloudcolor;
+function circlo() {
+if (hourslice>=7 && hourslice<19) {
+
+  cicleincolor = '';
+
+}
+
+else {
+
+    cicleincolor = '255, 255, 255';
+}
+
+return cicleincolor;
+
+
+}
 
 
 
-
-
-
-
-
-{
     for(var i=0+feelslikecircle/2; i <800; i=i+feelslikecircle) {
 
        for(var j=0+feelslikecircle/2; j<440; j=j+feelslikecircle) {
@@ -407,7 +435,7 @@ for(var x= -1000; x<6500; x=x+winSpeline) {
          noStroke();
          rotate(winDeg);
          rectMode(CENTER);
-         rect(0, 0, random(feelslikecircle/2, feelslikecircle/5), feelslikecircle, feelslikecircle/5);
+         rect(0, 0, random(feelslikecircle/2, feelslikecircle/5), feelslikecircle, feelslikecircle/4);
          pop();
 
         push();
@@ -422,7 +450,7 @@ for(var x= -1000; x<6500; x=x+winSpeline) {
             pop();
        }
       }
-}
+
 
     angleMode(DEGREES);
     fill('white');
@@ -431,8 +459,9 @@ for(var x= -1000; x<6500; x=x+winSpeline) {
     //text(message, 15, 410, 270, 350);
   //  text(footer, 15, 470, 270, 350);
   textSize(36);
-  noStroke();
+  stroke(100);
   textStyle(BOLD);
+  textLeading(38);
   text(message, 15, 15, 400, 600);
   textSize(18);
   textStyle(BOLD);
